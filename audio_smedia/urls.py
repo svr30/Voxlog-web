b8bd511ee309bc47549df3a01ccd1b33dfac9457
svr30 <145685135+svr30@users.noreply.github.com>
2026-04-27 21:38:45 +0530

    Update URL configuration to include app.urls

--- audio_smedia/urls.py
+++ audio_smedia/urls.py
@@ -1,4 +1,3 @@
-# audio_social/audio_social/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -6,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', include('app.urls')),
 ]
 
 if settings.DEBUG:
