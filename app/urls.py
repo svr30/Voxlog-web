f36016d83c0b1740b37a8d4ea98117e5ee3fdff9
svr30 <145685135+svr30@users.noreply.github.com>
2026-04-27 22:54:46 +0530

    Add names to URL patterns for better clarity

--- app/urls.py
+++ app/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('login/', views.login_view),
-    path('register/', views.register_view),
-    path('logout/', views.logout_view),
-    path('profile/', views.profile_view),
-    path('add-post/', views.add_post_view),
-    path('like/<int:post_id>/', views.like_post_view),
-    path('comment/<int:post_id>/', views.add_comment_view),
+
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+
+    path('profile/', views.profile_view, name='profile'),
+    path('add-post/', views.add_post_view, name='add_post'),
+
+    path('like/<int:post_id>/', views.like_post_view, name='like_post'),
+    path('comment/<int:post_id>/', views.add_comment_view, name='add_comment'),
 ]
