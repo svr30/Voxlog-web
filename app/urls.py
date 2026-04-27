3d4634bb1af29f829a49d35f3876a5d36394201c
svr30 <145685135+svr30@users.noreply.github.com>
2026-04-27 22:34:20 +0530

    Refactor URL patterns and remove admin paths

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('', views.home, name='home'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
