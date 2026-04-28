b315252d3c2ac34df337491aa29ae80fbd0a7239
svr30 <145685135+svr30@users.noreply.github.com>
2026-04-28 17:36:53 +0530

    Configure media file serving in urls.py
    Add static URL patterns for serving media files.

--- audio_smedia/urls.py
+++ audio_smedia/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+
+# THIS LINE FIXES YOUR IMAGE ISSUE
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
